285a21d8c7069f095fbf51a2f81a9d445962aa56
Vladimir <vpereverzev1200@gmail.com>
2024-03-23 20:44:34 +0300

    modified rcnn utils

--- Faster_RCNN/config.py
+++ Faster_RCNN/config.py
@@ -1,8 +1,8 @@
 import torch
 
-BATCH_SIZE = 8 # increase / decrease according to GPU memeory
+BATCH_SIZE = 10 # increase / decrease according to GPU memeory
 RESIZE_TO = 416 # resize the image for training and transforms
-NUM_EPOCHS = 10 # number of epochs to train for
+NUM_EPOCHS = 2 # number of epochs to train for
 NUM_WORKERS = 4
 ROOT = 'C:\\Users\ornst\VLADIMIR_USER\projects\kitty_detection\\'
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -14,16 +14,16 @@ TRAIN_LABELS_DIR = ROOT + 'image-detection-on-kitty-dataset\datasets\kitty\orig_
 # validation images and XML files directory
 VALID_DIR = 'data/Uno Cards.v2-raw.voc/valid'
 
-VALID_IMAGES_DIR = 'datasets\kitty\images\\train_65p'
-VALID_LABELS_DIR = 'datasets\kitty\orig_labels\\train_65p_intermediate'
+VALID_IMAGES_DIR = ROOT + 'image-detection-on-kitty-dataset\datasets\kitty\images\\train_25p'
+VALID_LABELS_DIR = ROOT + 'image-detection-on-kitty-dataset\datasets\kitty\orig_labels\\train_25p_intermediate'
 
 # classes: 0 index is reserved for background
-CLASSES = list(range(9))
+CLASSES = [str(i) for i in range(9)]
 
 NUM_CLASSES = len(CLASSES)
 
 # whether to visualize images after crearing the data loaders
-VISUALIZE_TRANSFORMED_IMAGES = True
+VISUALIZE_TRANSFORMED_IMAGES = False
 
 # location to save model and plots
 OUT_DIR = 'outputs'
--- Faster_RCNN/train.py
+++ Faster_RCNN/train.py
@@ -1,6 +1,6 @@
 from config import (
     DEVICE, NUM_CLASSES, NUM_EPOCHS, OUT_DIR,
-    VISUALIZE_TRANSFORMED_IMAGES, NUM_WORKERS,
+    VISUALIZE_TRANSFORMED_IMAGES, NUM_WORKERS, ROOT,
 )
 from model import create_model
 from custom_utils import Averager, SaveBestModel, save_model, save_loss_plot
@@ -13,14 +13,19 @@ from datasets import (
 import torch
 import matplotlib.pyplot as plt
 import time
+from torchmetrics.detection import MeanAveragePrecision
+
+
 
 plt.style.use('ggplot')
 
 # function for running training iterations
 def train(train_data_loader, model):
+
     print('Training')
     global train_itr
     global train_loss_list
+    model.train()
     
      # initialize tqdm progress bar
     prog_bar = tqdm(train_data_loader, total=len(train_data_loader))
@@ -54,7 +59,10 @@ def validate(valid_data_loader, model):
     print('Validating')
     global val_itr
     global val_loss_list
-    
+    global metric
+    global map_dict_list
+    model.eval()
+
     # initialize tqdm progress bar
     prog_bar = tqdm(valid_data_loader, total=len(valid_data_loader))
     
@@ -65,18 +73,25 @@ def validate(valid_data_loader, model):
         targets = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
         
         with torch.no_grad():
-            loss_dict = model(images, targets)
+            # loss_dict = model(images, targets)
 
-        losses = sum(loss for loss in loss_dict.values())
-        loss_value = losses.item()
-        val_loss_list.append(loss_value)
 
-        val_loss_hist.send(loss_value)
+            preds = model(images)
+
+        # losses = sum(loss for loss in loss_dict.values())
+        # loss_value = losses.item()
+        # val_loss_list.append(loss_value)
+        #
+        # val_loss_hist.send(loss_value)
+
+        metric.update(preds, targets)
+        map_dict_list.append(metric.compute())
 
         val_itr += 1
 
         # update the loss value beside the progress bar for each iteration
-        prog_bar.set_description(desc=f"Loss: {loss_value:.4f}")
+        # prog_bar.set_description(desc=f"Loss: {loss_value:.4f}")
+
     return val_loss_list
 
 if __name__ == '__main__':
@@ -98,12 +113,15 @@ if __name__ == '__main__':
     # initialize the Averager class
     train_loss_hist = Averager()
     val_loss_hist = Averager()
+    metric = MeanAveragePrecision()
+    metric.to(DEVICE)
     train_itr = 1
     val_itr = 1
     # train and validation loss lists to store loss values of all...
     # ... iterations till ena and plot graphs for all iterations
     train_loss_list = []
     val_loss_list = []
+    map_dict_list = []
 
     # name to save the trained model with
     MODEL_NAME = 'model'
@@ -126,7 +144,7 @@ if __name__ == '__main__':
 
         # start timer and carry out training and validation
         start = time.time()
-        train_loss = train(train_loader, model)
+        # train_loss = train(train_loader, model)
         val_loss = validate(valid_loader, model)
         print(f"Epoch #{epoch+1} train loss: {train_loss_hist.value:.3f}")   
         print(f"Epoch #{epoch+1} validation loss: {val_loss_hist.value:.3f}")   
@@ -142,7 +160,24 @@ if __name__ == '__main__':
         save_model(epoch, model, optimizer)
 
         # save loss plot
-        save_loss_plot(OUT_DIR, train_loss, val_loss)
+        # save_loss_plot(OUT_DIR, train_loss, val_loss)
         
         # sleep for 5 seconds after each epoch
         time.sleep(5)
+
+    all_epoch_map_stats = dict()
+    for i, elem in enumerate(map_dict_list):
+        all_epoch_map_stats[i] = elem
+
+    STATS_DIR = ROOT + 'image-detection-on-kitty-dataset\Faster_RCNN\\runs\\run2\\'
+    try:
+        stats_file = open(STATS_DIR + 'map_stats.txt', 'wt')
+        stats_file.write(str(all_epoch_map_stats))
+        stats_file.close()
+
+    except:
+        print("Unable to write to file")
+
+    fig_, ax_ = metric.plot()
+    PLOT_DIR = ROOT + 'image-detection-on-kitty-dataset\Faster_RCNN\\runs\\run2\\'
+    fig_.savefig(PLOT_DIR + 'progress.png')
